56eb9cf2e90edc69b4d0b8e5106d509fe490ea9c
Sebastian Cruz <scruz17@lion.lmu.edu>
2023-02-16 14:11:56 -0800

    changed print to "report", has errors in grammar test

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -3,28 +3,28 @@ import fs from "fs"
 import ohm from "ohm-js"
 
 const syntaxChecks = [
-  ["all numeric literal forms", "print(8 * 89.123)"],
-  ["complex expressions", "print(83 * ((((-((((13 / 21)))))))) + 1 - 0)"],
-  ["all unary operators", "print (-3) print (!false)"],
-  ["all binary operators", "print x && y || z * 1 / 2 ** 3 + 4 < 5"],
+  ["all numeric literal forms", "report(8 * 89.123)"],
+  ["complex expressions", "report(83 * ((((-((((13 / 21)))))))) + 1 - 0)"],
+  ["all unary operators", "report (-3) report (!false)"],
+  ["all binary operators", "report x && y || z * 1 / 2 ** 3 + 4 < 5"],
   ["all arithmetic operators", "varus x = (!3) * 2 + 4 - (-7.3) * 8 ** 13 / 1"],
   ["all relational operators", "varus x = 1<(2<=(3==(4!=(5 >= (6>7)))))"],
   ["all logical operators", "varus x = true && false || (!false)"],
-  ["the conditional operator", "print x ? y : z"],
-  ["end of program inside comment", "print(0) cR yay"],
-  ["comments with no text are ok", "print(1)cR\nprint(0)cR"],
+  ["the conditional operator", "report x ? y : z"],
+  ["end of program inside comment", "report(0) cR yay"],
+  ["comments with no text are ok", "report(1)cR\nreport(0)cR"],
   ["non-Latin letters in identifiers", "コンパイラ = 100"],
 ]
 
 const syntaxErrors = [
   ["non-letter in an identifier", "ab😭c = 2", /Line 1, col 3/],
   ["malformed number", "x= 2.", /Line 1, col 6/],
-  ["a missing right operand", "print(5 -", /Line 1, col 10/],
-  ["a non-operator", "print(7 * ((2 _ 3)", /Line 1, col 15/],
+  ["a missing right operand", "report(5 -", /Line 1, col 10/],
+  ["a non-operator", "report(7 * ((2 _ 3)", /Line 1, col 15/],
   ["an expression starting with a )", "x = )", /Line 1, col 5/],
   ["a statement starting with expression", "x * 5", /Line 1, col 3/],
-  ["an illegal statement on line 2", "print(5)\nx * 5", /Line 2, col 3/],
-  ["a statement starting with a )", "print(5)\n) * 5", /Line 2, col 1/],
+  ["an illegal statement on line 2", "report(5)\nx * 5", /Line 2, col 3/],
+  ["a statement starting with a )", "report(5)\n) * 5", /Line 2, col 1/],
   ["an expression starting with a *", "x = * 71", /Line 1, col 5/],
 ]
 
