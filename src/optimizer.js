9dc6f9b809082514b4988d9ab5f175e63eed11bb
Connor Savage <75349013+connorsavage@users.noreply.github.com>
2023-04-24 16:46:44 -0700

    constant folding optimization
    updated the optimizer by adding constant folding

--- src/optimizer.js
+++ src/optimizer.js
@@ -1,3 +1,45 @@
+function constantFoldBinaryExpression(node) {
+  const left = constantFold(node.left);
+  const right = constantFold(node.right);
+
+  if (typeof left === "number" && typeof right === "number") {
+    switch (node.op) {
+      case "+":
+        return left + right;
+      case "-":
+        return left - right;
+      case "*":
+        return left * right;
+      case "/":
+        return left / right;
+      default:
+        return {
+          ...node,
+          left,
+          right,
+        };
+    }
+  }
+
+  return {
+    ...node,
+    left,
+    right,
+  };
+}
+
+function constantFold(node) {
+  if (!node) {
+    return node;
+  }
+
+  if (node.type === "BinaryExpression") {
+    return constantFoldBinaryExpression(node);
+  }
+
+  return node;
+}
+
 export default function optimize(node) {
-  return node
+  return constantFold(node);
 }
